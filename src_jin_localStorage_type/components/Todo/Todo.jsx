fded0a3ef63db7f3751a2096090532d3f499bd3c
lhjwork <jin.come.up.business@gmail.com>
2023-09-12 11:18:20 +0900

    localStorage my working park

---
+++ src_jin_localStorage_type/components/Todo/Todo.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { FaTrashAlt } from "react-icons/fa";
+import styles from "./Todo.module.css";
+
+export default function ({ todo, onUpdate, onDelete }) {
+  const { text, status } = todo;
+
+  const handleChange = (e) => {
+    const status = e.target.checked ? "completed" : "active";
+    onUpdate({ ...todo, status });
+  };
+
+  const handleDelete = () => onDelete(todo);
+  return (
+    <li className={styles.todo}>
+      <input
+        className={styles.checkbox}
+        type="checkbox"
+        id="checkbox"
+        checked={status === "completed"}
+        onChange={handleChange}
+      />
+      <label className={styles.text}>{todo.text}</label>
+      <span className={styles.icon}>
+        <button className={styles.button} onClick={handleDelete}>
+          <FaTrashAlt />
+        </button>
+      </span>
+    </li>
+  );
+}
---
+++ src_jin_localStorage_type/components/TodoList/TodoList.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import AddTodo from "../AddTodo/AddTodo";
+import Todo from "../Todo/Todo";
+import styles from "./TodoList.module.css";
+
+export default function TodoList({ filter }) {
+  const [todos, setTodos] = useState([
+    { id: "123", text: "장보기", status: "active" },
+    { id: "124", text: "공부하기", status: "active" },
+  ]);
+
+  useEffect(() => {
+    localStorage.setItem("obj", JSON.stringify(todos));
+  }, [todos]);
+
+  const test2 = JSON.parse(localStorage.getItem("obj"));
+
+  console.log("resr", test2);
+  const handleUpdate = (updated) =>
+    setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
+  const handleAdd = (todo) => {
+    setTodos([...todos, todo]);
+  };
+
+  const handleDelete = (deleted) =>
+    setTodos(todos.filter((t) => t.id !== deleted.id));
+
+  const filtered = getFilteredItems(filter);
+
+  return (
+    <section className={styles.container}>
+      <ul className={styles.list}>
+        {filtered.map((item) => (
+          <Todo
+            key={item.id}
+            todo={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
+        ))}
+      </ul>
+      <AddTodo onAdd={handleAdd} />
+    </section>
+  );
+}
+
+function getFilteredItems(filter) {
+  const todos = JSON.parse(localStorage.getItem("obj"));
+
+  if (filter === "all") {
+    return todos;
+  }
+
+  return todos.filter((todo) => todo.status === filter);
+}
